f81fbdb843d615a157aaf131c532c3ffa89be720
LavenderMorara <chiyamorara@gmail.com>
2025-03-07 13:54:10 +0300

    did some object method practice

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee={
+    name:"Tye",
+    streetAddress:21009
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let newEmployee;
+    newEmployee={...employee, [key]:value}
+   
+   return newEmployee
+}
+//console.log(updateEmployeeWithKeyAndValue(employee,"age",31))
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+   employee[key]=value
+    return employee
+}
+//console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "gender", "male"))
+
+function deleteFromEmployeeByKey(employee,key){
+    let newEmployee;
+    newEmployee={...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee
+}
